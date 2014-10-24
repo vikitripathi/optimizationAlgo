9694ac0f37e0054b4118c008c51c36e87ea32b0b
Abhishek Dutt <duttabhishekb209@gmail.com>
2014-10-24 23:34:04 +0530

    initial setup: optimization algorithm in python for group travel problem

---
+++ optimization.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 24 23:11:26 2014
+
+@author: Abhishek Dutt
+"""
+
+import time
+import random
+import math
+
+people=[('Seymour','BOS'),
+        ('Franny','DAL'),
+        ('Zooey','CAK'),
+        ('Walt','MIA'),
+        ('Buddy','ORD'),
+        ('Les','OMA')]
+        
+# LaGuardia airport in New York
+destination='LGA'
