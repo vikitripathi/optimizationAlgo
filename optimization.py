ab6d35671b30aee56cacc91d54cc1e1fbd8f16aa
Abhishek Dutt <duttabhishekb209@gmail.com>
2014-10-29 19:00:10 +0530

    implemented genetic algo on the flight problem

--- optimization.py
+++ optimization.py
@@ -193,6 +193,11 @@ are better (have a lower cost function).
 #domain
 #[(0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8), (0, 8)]
 # for j in range(12)   it will be 0,1,2,.... 11
+"""
+sol=[random.randint(0,8)for i in range(12)] 
+sol
+[1, 8, 1, 7, 2, 6, 8, 1, 8, 5, 1, 3]
+"""
 def hillclimb(domain,costf):
     # Create a random solution
     sol=[random.randint(domain[i][0],domain[i][1])    
@@ -245,9 +250,60 @@ that one of them will be close to the global minimum.
  
       
 #genetic algorithms
+"""
+popsize: is the population size
+mutprod:the probability that a newer member of the population will be a mutation or a crossover
+elite:fraction of solution that are considerd good and are alowed to pass to neat gen
+maxiter: the no of gen to run
+"""        
+def geneticoptimize(domain,costf,popsize=50,step=1,mutprod=0.2,elite=0.2,maxiter=100):
+    # Mutation Operation
+    def mutate(vec):
+        i=random.randint(0,len(domain)-1)
+        if random.random( )<0.5 and vec[i]>domain[i][0]:
+            return vec[0:i]+[vec[i]-step]+vec[i+1:]
+        elif vec[i]<domain[i][1]:
+            return vec[0:i]+[vec[i]+step]+vec[i+1:]    
+
+    # Crossover Operation
+    def crossover(r1,r2):
+        i=random.randint(1,len(domain)-2)
+        return r1[0:i]+r2[i:]                                                      
+
+    # Build the initial population
+    pop=[]
+    for i in range(popsize):
+        #vec is a random solution
+        vec=[random.randint(domain[i][0],domain[i][1])
+            for i in range(len(domain))]
+        pop.append(vec)
         
+    # How many winners from each generation?
+    topelite=int(elite*popsize)
 
-                                                      
 
-    
-    
+    # Main loop
+    for i in range(maxiter):
+        scores=[(costf(v),v) for v in pop]
+        scores.sort( )
+        ranked=[v for (s,v) in scores]
+
+        # Start with the pure winners
+        pop=ranked[0:topelite]
+
+        # Add mutated and bred forms of the winners
+        while len(pop)<popsize:
+            if random.random( )< mutprod:
+                # Mutation
+                c=random.randint(0,topelite)
+                pop.append(mutate(ranked[c]))
+            else:
+                # Crossover
+                c1=random.randint(0,topelite)
+                c2=random.randint(0,topelite)
+                pop.append(crossover(ranked[c1],ranked[c2]))
+
+        # Print current best score
+        print scores[0][0]
+
+    return scores[0][1]    
